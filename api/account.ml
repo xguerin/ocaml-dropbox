8c17badaeffe1721feb7b8ab1c6cabe72f25f173
Xavier R. Guérin <github@applepine.org>
2020-11-02 11:07:43 -0500

    Implement account/set_profile_photo

--- api/account.ml
+++ api/account.ml
@@ -1,9 +1,76 @@
 open Endpoint
 
 module Make (C : Cohttp_lwt.S.Client) = struct
-  let set_profile_photo_uri = Root.api "/account/set_profile_photo"
+  (*
+   * Protocol.
+   *)
 
-  let set_profile_photo (_ : Session.Type.t) =
+  module Protocol = struct
+    module PhotoSourceArg = struct
+      module Type = struct
+        type t = Base64_data of string [@@deriving dropbox]
+      end
+
+      module Json = Json.Make (Type)
+    end
+
+    module SetProfilePhotoArg = struct
+      module Type = struct
+        type t = {photo : PhotoSourceArg.Type.t} [@@deriving yojson]
+      end
+
+      module Json = Json.Make (Type)
+    end
+
+    module SetProfilePhotoResult = struct
+      module Type = struct
+        type t = {profile_photo_url : string} [@@deriving yojson]
+      end
+
+      module Json = Json.Make (Type)
+    end
+
+    module SetProfilePhotoError = struct
+      module Type = struct
+        type t =
+          | File_type_error
+          | File_size_error
+          | Dimension_error
+          | Thumbnail_error
+          | Transient_error
+        [@@deriving dropbox]
+      end
+
+      module Json = Json.Make (Type)
+
+      let to_string = function
+        | Type.File_type_error -> "File type error"
+        | Type.File_size_error -> "File size error"
+        | Type.Dimension_error -> "Dimension error"
+        | Type.Thumbnail_error -> "Thumbnail error"
+        | Type.Transient_error -> "Transient error"
+    end
+  end
+
+  (*
+   * Set profile photo.
+   *)
+
+  module SetProfilePhoto = struct
+    module Arg = Protocol.SetProfilePhotoArg
+    module Result = Protocol.SetProfilePhotoResult
+    module Error = Error.S (Protocol.SetProfilePhotoError)
+
+    module Info = struct
+      let uri = Root.api "/account/set_profile_photo"
+    end
+
+    module Fn = RemoteProcedureCall.Function (C) (Arg) (Result) (Error) (Info)
+  end
+
+  let set_profile_photo ~session data =
+    let request = SetProfilePhoto.Arg.Type.{photo = Base64_data data}
+    and headers = Session.headers session in
     let module Error = Error.S (Error.Void) in
-    Lwt.return_error Error.Not_implemented
+    SetProfilePhoto.Fn.call ~headers request
 end
